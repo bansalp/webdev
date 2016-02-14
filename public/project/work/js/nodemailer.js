21cfaa472b1d69a6135a9aa3e704faea3ddb3266
bansalp <bansalp@ccs.neu.edu>
2016-02-14 04:00:01 -0500

    Added nodemailer.js to send emails on the fly

---
+++ public/project/work/js/nodemailer.js
@@ -0,0 +1,31 @@
+var nodemailer = require('nodemailer');
+
+var smtpConfig = {
+    host: 'smtp.gmail.com',
+    port: 465,
+    secure: true, // use SSL
+    auth: {
+        user: 'foo@blurdybloop.com',
+        pass: 'pass'
+    }
+};
+
+// create reusable transporter object using the default SMTP transport
+var transporter = nodemailer.createTransport(smtpConfig);
+
+// setup e-mail data with unicode symbols
+var mailOptions = {
+    from: 'Fred Foo <foo@blurdybloop.com>', // sender address
+    to: 'bar@blurdybloop.com, baz@blurdybloop.com', // list of receivers
+    subject: 'Hello', // Subject line
+    text: 'Hello world', // plaintext body
+    html: '<b>Hello world</b>' // html body
+};
+
+// send mail with defined transport object
+transporter.sendMail(mailOptions, function(error, info){
+    if(error){
+        return console.log(error);
+    }
+    console.log('Message sent: ' + info.response);
+});
