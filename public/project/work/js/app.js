6cbd4d9c65ec76585d0648cdeae0f4a080a8b479
bansalp <bansalp@ccs.neu.edu>
2016-02-16 20:53:00 -0500

    Added logic for search a movie by title

--- public/project/work/js/app.js
+++ public/project/work/js/app.js
@@ -2,13 +2,73 @@
 
     $(init);
 
+    var $movieTitleTxt;
+    var $searchMovieBtn;
     var $results;
+    var searchUrl = "http://api.themoviedb.org/3/search/movie?api_key=0989313de69b690eda213c2c6387e038&language=en&query=TITLE&page=PAGE";
     var popularMovieUrl = "http://api.themoviedb.org/3/discover/movie?api_key=0989313de69b690eda213c2c6387e038&sort_by=popularity.desc&language=en";
     var imageUrl = "http://image.tmdb.org/t/p/original/IMAGEPATH";
 
     function init() {
+        $movieTitleTxt = $("#movieTitleTxt");
+        $searchMovieBtn = $("#searchMovieBtn");
         $results = $(".results");
+        $searchMovieBtn.click(searchMovie);
         popularMovies();
+    }
+
+    function searchMovie() {
+        var movieTitle = $movieTitleTxt.val();
+        var url = searchUrl
+            .replace("TITLE", movieTitle)
+            .replace("PAGE", 1);
+
+        $.ajax({
+            url: url,
+            success: renderMovieList
+        });
+    }
+
+    function renderMovieList(response) {
+        $results.empty();
+
+        var movies = response.results;
+
+        var m;
+        for (m in movies) {
+            var movie = movies[m];
+
+            var title = movie.title;
+            var id = movie.id;
+            var image_path = movie.backdrop_path;
+            var image = imageUrl
+                .replace("IMAGEPATH", image_path);
+
+            var $div1 = $("<div>");
+            $div1.addClass("col-sm-6 col-md-4");
+
+            var $div2 = $("<div>");
+            $div2.addClass("thumbnail");
+
+            var $img = $("<img>")
+                .attr("src", image)
+                .attr("id", id)
+                .addClass("img-responsive")
+                .click(searchMovieDetails);
+
+            var $div3 = $("<div>");
+            $div3.addClass("caption");
+
+            var $h3 = $("<h3>");
+            $h3.append(title);
+
+            $div3.append($h3);
+            $div2.append($img).append($div3);
+            $div1.append($div2);
+            $div1.append($div1);
+
+            $results.append($div1);
+        }
     }
 
     function popularMovies() {
