90c42553ffa71928c88ae3524b830a4e127e55cb
bansalp <bansalp@ccs.neu.edu>
2016-02-16 21:11:56 -0500

    Corrected mistakes for search page

--- public/project/work/js/app.js
+++ public/project/work/js/app.js
@@ -4,6 +4,7 @@
 
     var $movieTitleTxt;
     var $searchMovieBtn;
+    var $h3PageHeader;
     var $results;
     var searchUrl = "http://api.themoviedb.org/3/search/movie?api_key=0989313de69b690eda213c2c6387e038&language=en&query=TITLE&page=PAGE";
     var popularMovieUrl = "http://api.themoviedb.org/3/discover/movie?api_key=0989313de69b690eda213c2c6387e038&sort_by=popularity.desc&language=en";
@@ -12,12 +13,14 @@
     function init() {
         $movieTitleTxt = $("#movieTitleTxt");
         $searchMovieBtn = $("#searchMovieBtn");
+        $h3PageHeader = $(".h3-page-header");
         $results = $(".results");
         $searchMovieBtn.click(searchMovie);
         popularMovies();
     }
 
     function searchMovie() {
+        $h3PageHeader.html("Search Results")
         var movieTitle = $movieTitleTxt.val();
         var url = searchUrl
             .replace("TITLE", movieTitle)
@@ -36,42 +39,13 @@
 
         var m;
         for (m in movies) {
-            var movie = movies[m];
-
-            var title = movie.title;
-            var id = movie.id;
-            var image_path = movie.backdrop_path;
-            var image = imageUrl
-                .replace("IMAGEPATH", image_path);
-
-            var $div1 = $("<div>");
-            $div1.addClass("col-sm-6 col-md-4");
-
-            var $div2 = $("<div>");
-            $div2.addClass("thumbnail");
-
-            var $img = $("<img>")
-                .attr("src", image)
-                .attr("id", id)
-                .addClass("img-responsive")
-                .click(searchMovieDetails);
-
-            var $div3 = $("<div>");
-            $div3.addClass("caption");
-
-            var $h3 = $("<h3>");
-            $h3.append(title);
-
-            $div3.append($h3);
-            $div2.append($img).append($div3);
-            $div1.append($div2);
-            $div1.append($div1);
-
-            $results.append($div1);
+            addMovie(movies, m);
         }
     }
 
     function popularMovies() {
+        $h3PageHeader.html("Popular Movies")
+
         $.ajax({
             url: popularMovieUrl,
             success: renderPopularMovieList
@@ -88,44 +62,47 @@
         for (m in movies) {
             if (n < 18) {
                 n = n + 1;
-
-                var movie = movies[m];
-
-                var title = movie.title;
-                var id = movie.id;
-                var image_path = movie.backdrop_path;
-                var image = imageUrl
-                    .replace("IMAGEPATH", image_path);
-
-                var $div1 = $("<div>");
-                $div1.addClass("col-sm-6 col-md-4");
-
-                var $div2 = $("<div>");
-                $div2.addClass("thumbnail");
-
-                var $img = $("<img>")
-                    .attr("src", image)
-                    .attr("id", id)
-                    .addClass("img-responsive")
-                    .click(searchMovieDetails);
-
-                var $div3 = $("<div>");
-                $div3.addClass("caption");
-
-                var $h3 = $("<h3>");
-                $h3.append(title);
-
-                $div3.append($h3);
-                $div2.append($img).append($div3);
-                $div1.append($div2);
-                $div1.append($div1);
-
-                $results.append($div1);
+                addMovie(movies, m);
             }
             else {
                 break;
             }
         }
+    }
+
+    function addMovie(movies, m) {
+        var movie = movies[m];
+
+        var title = movie.title;
+        var id = movie.id;
+        var image_path = movie.backdrop_path;
+        var image = imageUrl
+            .replace("IMAGEPATH", image_path);
+
+        var $div1 = $("<div>");
+        $div1.addClass("col-sm-6 col-md-4");
+
+        var $div2 = $("<div>");
+        $div2.addClass("thumbnail");
+
+        var $img = $("<img>")
+            .attr("src", image)
+            .attr("id", id)
+            .addClass("img-responsive")
+            .click(searchMovieDetails);
+
+        var $div3 = $("<div>");
+        $div3.addClass("caption");
+
+        var $h3 = $("<h3>");
+        $h3.append(title);
+
+        $div3.append($h3);
+        $div2.append($img).append($div3);
+        $div1.append($div2);
+        $div1.append($div1);
+
+        $results.append($div1);
     }
 
     function searchMovieDetails (event) {
