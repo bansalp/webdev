673eff4cad4c42ee5db2433fea9bd3e2a25b3fa0
bansalp <bansalp@ccs.neu.edu>
2016-02-22 00:43:19 -0500

    Updated header and sidebar for highlighting links

--- public/assignment/views/header/header.controller.js
+++ public/assignment/views/header/header.controller.js
@@ -4,8 +4,12 @@
         .module("FormBuilderApp")
         .controller("HeaderController", HeaderController);
 
-    function HeaderController() {
+    function HeaderController($scope, $location) {
+        $scope.isLoggedIn = isLoggedIn;
 
+        function isLoggedIn() {
+            return ($location.url() != '/home' && $location.url() != '/login' && $location.url() != '/register');
+        }
     }
 
 })();
