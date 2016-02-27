4d83a04dc0e233fe82d32b4c2db99382f3732910
bansalp <bansalp@ccs.neu.edu>
2016-02-27 15:24:08 -0500

    Implemented register, login and forgot-password pages

---
+++ public/experiments/project/js/app.js
@@ -0,0 +1,6 @@
+(function () {
+
+    angular
+        .module("MovieTimeApp", ["ngRoute", "ui.router"]);
+
+})();
---
+++ public/experiments/project/js/config.js
@@ -0,0 +1,29 @@
+(function () {
+
+    angular
+        .module("MovieTimeApp")
+        .config(configuration);
+
+    function configuration($routeProvider) {
+        $routeProvider
+            .when("/home", {
+                templateUrl: "views/home/home.view.html"
+            })
+            .when("/header", {
+                templateUrl: "views/header/header.view.html"
+            })
+            .when("/login", {
+                templateUrl: "views/login/login.view.html"
+            })
+            .when("/forgot-password", {
+                templateUrl: "views/login/forgot-password/forgot-password.view.html"
+            })
+            .when("/register", {
+                templateUrl: "views/register/register.view.html"
+            })
+            .otherwise({
+                redirectTo : "/home"
+            })
+    }
+
+})();
