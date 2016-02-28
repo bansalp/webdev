2db3b9b69dfd85a2072cb247e3100edec8d76ebf
bansalp <bansalp@ccs.neu.edu>
2016-02-27 19:52:18 -0500

    Implemented search results page

--- public/experiments/project/services/movie.service.client.js
+++ public/experiments/project/services/movie.service.client.js
@@ -7,11 +7,13 @@
 
     function MovieService($http) {
         var popularMovieUrl = "http://api.themoviedb.org/3/discover/movie?api_key=0989313de69b690eda213c2c6387e038&sort_by=popularity.desc&language=en";
+        var searchUrl = "http://api.themoviedb.org/3/search/movie?api_key=0989313de69b690eda213c2c6387e038&language=en&query=TITLE&page=PAGE";
         var imageUrl = "http://image.tmdb.org/t/p/original/";
 
         var api = {
             findPopularMovies: findPopularMovies,
-            getImageURL: getImageURL
+            getImageURL: getImageURL,
+            getMoviesByTitle: getMoviesByTitle
         };
         return api;
 
@@ -22,6 +24,14 @@
 
         function getImageURL(callback) {
             callback(imageUrl);
+        }
+
+        function getMoviesByTitle(movie, callback) {
+            var url = searchUrl
+                .replace("TITLE", movie.title)
+                .replace("PAGE", 1);
+            $http.get(url)
+                .success(callback);
         }
     }
 })();
--- public/experiments/project/views/home/home.controller.js
+++ public/experiments/project/views/home/home.controller.js
@@ -7,9 +7,9 @@
     function HomeController($scope, MovieService) {
 
         $scope.getMovieDetailsById = getMovieDetailsById;
+        $scope.getMoviesByTitle = getMoviesByTitle;
 
         $scope.pageHeader = "Popular Movies";
-
         MovieService.findPopularMovies(function (response) {
             console.log(response);
             $scope.movies = response.results;
@@ -21,6 +21,14 @@
 
         function getMovieDetailsById(movieId) {
             alert(movieId);
+        }
+
+        function getMoviesByTitle(movie) {
+            $scope.pageHeader = "Search Results";
+            MovieService.getMoviesByTitle(movie, function(response){
+                console.log(response);
+                $scope.movies = response.results;
+            });
         }
     }
 
