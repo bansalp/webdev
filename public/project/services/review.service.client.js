063c16103e9387b627ad532906d1fccb3c0bf603
bansalp <bansalp@ccs.neu.edu>
2016-03-10 21:23:00 -0500

    Updated reviews section to change the view on small devices

--- public/project/services/review.service.client.js
+++ public/project/services/review.service.client.js
@@ -11,7 +11,7 @@
                 "_id": 1,
                 "title": "Lorem Ipsum 1",
                 "description": "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
-                "timestamp": "August 25, 2014 at 9:30 PM",
+                "timestamp": "Mar 10, 2016",
                 "movieId": 293660,
                 "userId": 1,
                 "commentIds": [1, 2, 3]
@@ -20,7 +20,7 @@
                 "_id": 2,
                 "title": "Lorem Ipsum 2",
                 "description": "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
-                "timestamp": "August 26, 2014 at 9:30 PM",
+                "timestamp": "Mar 10, 2016",
                 "movieId": 293660,
                 "userId": 2,
                 "commentIds": [4, 5]
@@ -29,7 +29,7 @@
                 "_id": 3,
                 "title": "Lorem Ipsum 3",
                 "description": "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
-                "timestamp": "August 27, 2014 at 9:30 PM",
+                "timestamp": "Mar 10, 2016",
                 "movieId": 76341,
                 "userId": 3,
                 "commentIds": []
@@ -65,7 +65,7 @@
                 "userId": 1,
                 "commentIds": []
             };
-            reviews.unshift(newReview);
+            reviews.push(newReview);
             callback();
         }
 
