48a0c6171d8328a8ca9373a2b44310f34d52c6ae
bansalp <bansalp@ccs.neu.edu>
2016-03-17 00:16:19 -0400

    Implemented ng-switch to view all the fields of different types on the view

--- public/assignment3/client/views/forms/fields.controller.js
+++ public/assignment3/client/views/forms/fields.controller.js
@@ -9,6 +9,33 @@
         var vm = this;
 
         vm.addField = addField;
+        
+        var fieldTypesDefault = [
+            {"_id": null, "label": "New Text Field", "type": "TEXT", "placeholder": "New Field"},
+            {"_id": null, "label": "New Text Field", "type": "TEXTAREA", "placeholder": "New Field"},
+            {"_id": null, "label": "New Date Field", "type": "DATE"},
+            {
+                "_id": null, "label": "New Dropdown", "type": "OPTIONS", "options": [
+                {"label": "Option 1", "value": "OPTION_1"},
+                {"label": "Option 2", "value": "OPTION_2"},
+                {"label": "Option 3", "value": "OPTION_3"}
+            ]
+            },
+            {
+                "_id": null, "label": "New Checkboxes", "type": "CHECKBOXES", "options": [
+                {"label": "Option A", "value": "OPTION_A"},
+                {"label": "Option B", "value": "OPTION_B"},
+                {"label": "Option C", "value": "OPTION_C"}
+            ]
+            },
+            {
+                "_id": null, "label": "New Radio Buttons", "type": "RADIOS", "options": [
+                {"label": "Option X", "value": "OPTION_X"},
+                {"label": "Option Y", "value": "OPTION_Y"},
+                {"label": "Option Z", "value": "OPTION_Z"}
+            ]
+            }
+        ];
 
         function init() {
             vm.fieldTypes = ["Single Line Text Field", "Multi Line Text Field", "Date Field", "Dropdown Field", "Checkboxes Field", "Radio Buttons Field"];
@@ -27,8 +54,7 @@
         init();
 
         function addField(fieldType) {
-            if (fieldType == -1)
-            {
+            if (fieldType == -1) {
                 alert("Please select a value for Add New Field");
                 return;
             }
