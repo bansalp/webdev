8ae5726b6f42b59d4a372ed1f4fc409999fca827
bansalp <bansalp@ccs.neu.edu>
2016-03-18 20:41:13 -0400

    Replaced existing assignment folder with assignment3 folder

--- server.js
+++ server.js
@@ -17,6 +17,6 @@ app.use(express.static(__dirname + '/public'));
 var ipaddress = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1';
 var port = process.env.OPENSHIFT_NODEJS_PORT || 3000;
 
-require("./public/assignment3/server/app.js")(app, uuid);
+require("./public/assignment/server/app.js")(app, uuid);
 
 app.listen(port, ipaddress);
