4aeebba4da0bf76c1e8b04c6396c570a71ee4ebd
bansalp <bansalp@ccs.neu.edu>
2016-04-10 15:11:33 -0400

    Partial implementation of following tab under profile page

--- public/project/client/views/users/profile/following.controller.js
+++ public/project/client/views/users/profile/following.controller.js
@@ -4,8 +4,10 @@
         .module("MovieTimeApp")
         .controller("FollowingController", FollowingController);
 
-    function FollowingController(UserService) {
+    function FollowingController($q, $stateParams, UserService) {
         var vm = this;
+
+        vm.navigateUserId = $stateParams.userId;
 
         function init() {
             UserService
@@ -14,18 +16,58 @@
                     var user = response.data;
                     if (user) {
                         vm.user = user;
-                        return UserService.findAllFollowingUsers(vm.user._id);
+                        vm.loggedInUserId = user._id;
+                        return UserService.findAllFollowingUsers(vm.navigateUserId);
                     }
                 })
                 .then(function (response) {
                     var users = response.data;
                     if (users) {
                         vm.users = users;
+                        isAlreadyFollowing(vm.users);
                     }
                 });
         }
 
         init();
+
+        function isAlreadyFollowing(users) {
+            var promiseArray = [];
+            var result = [];
+
+            users.forEach(function (element, index, array) {
+                promiseArray
+                    .push(
+                        UserService
+                            .isAlreadyFollowing(vm.loggedInUserId, element._id)
+                            .then(
+                                function (response) {
+                                    var user = element;
+
+                                    if (user._id != vm.loggedInUserId) {
+                                        if (response.data) {
+                                            user.alreadyFollowing = true;
+                                        }
+                                        else {
+                                            user.alreadyFollowing = false;
+                                        }
+                                    }
+                                    else {
+                                        user.itsMe = true;
+                                    }
+
+                                    result.push(user);
+                                },
+                                function (err) {
+                                    console.log(err);
+                                })
+                    );
+            });
+
+            $q.all(promiseArray).then(function () {
+                vm.users = result;
+            });
+        }
     }
 
 })();
