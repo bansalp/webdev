65db4ed91a6330a951358ed85eb2676fe70b78b0
bansalp <bansalp@ccs.neu.edu>
2016-04-12 22:53:30 -0400

    Added credits url to movie service client side - project

--- public/project/client/services/movie.service.client.js
+++ public/project/client/services/movie.service.client.js
@@ -8,6 +8,7 @@
     function MovieService($http) {
         var popularMovieUrl = "http://api.themoviedb.org/3/discover/movie?api_key=0989313de69b690eda213c2c6387e038&sort_by=popularity.desc&language=en";
         var genreListUrl = "http://api.themoviedb.org/3/genre/movie/list?api_key=0989313de69b690eda213c2c6387e038&language=en";
+        var creditsUrl = "http://api.themoviedb.org/3/movie/MOVIEID/credits?api_key=0989313de69b690eda213c2c6387e038&language=en";
         var searchUrl = "http://api.themoviedb.org/3/search/movie?api_key=0989313de69b690eda213c2c6387e038&language=en&query=TITLE&page=PAGE";
         var detailsUrl = "http://api.themoviedb.org/3/movie/ID?api_key=0989313de69b690eda213c2c6387e038&language=en";
         var imageUrl = "http://image.tmdb.org/t/p/original/";
