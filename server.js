15512eee0130d27c3ee9bf106b9bc42b9cdb843c
bansalp <bansalp@ccs.neu.edu>
2016-04-13 01:05:19 -0400

    Added environment variable to handle secret session

--- server.js
+++ server.js
@@ -25,7 +25,7 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.use(multer());
 
 app.use(session({
-    secret: 'this is the secret',
+    secret: process.env.SESSION_SECRET,
     resave: true,
     saveUninitialized: true
 }));
