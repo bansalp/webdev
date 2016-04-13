5694a57819f482e6d01f8e2726e85794877e7127
bansalp <bansalp@ccs.neu.edu>
2016-04-12 23:37:04 -0400

    Configured passport modules in server.js

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 var multer = require('multer');
-var cookieParser = require('cookie-parser');
-var session = require('express-session');
+var passport      = require('passport');
+var cookieParser  = require('cookie-parser');
+var session       = require('express-session');
 var uuid = require('node-uuid');
 var mongoose = require("mongoose");
+
 var connection_string = "mongodb://localhost/webdevelopment";
 if (process.env.OPENSHIFT_MONGODB_DB_PASSWORD) {
     connection_string = process.env.OPENSHIFT_MONGODB_DB_USERNAME + ":" +
@@ -21,8 +23,16 @@ var app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(multer());
-app.use(session({secret: "ParthBansal"}));
+
+app.use(session({
+    secret: 'this is the secret',
+    resave: true,
+    saveUninitialized: true
+}));
 app.use(cookieParser());
+app.use(passport.initialize());
+app.use(passport.session());
+
 app.use(express.static(__dirname + '/public'));
 
 var ipaddress = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1';
