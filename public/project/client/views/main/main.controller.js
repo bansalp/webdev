6f3098eb89b0bbeb086d188ee825fd69d3fcbd98
bansalp <bansalp@ccs.neu.edu>
2016-04-16 02:21:51 -0400

    Fixed scrolling issue on details page

--- public/project/client/views/main/main.controller.js
+++ public/project/client/views/main/main.controller.js
@@ -11,6 +11,11 @@
                 function (event, viewConfig) {
                     $rootScope.isToggleMenuVisible = $state.includes("profile");
                 });
+
+            $rootScope.$on('$stateChangeSuccess', function () {
+                document.body.scrollTop = 0;
+                document.documentElement.scrollTop = 0;
+            });
         }
 
         init();
