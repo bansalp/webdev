ad0ecca7580b7000fd2f6479af27507012f645dd
bansalp <bansalp@ccs.neu.edu>
2016-04-15 21:54:26 -0400

    Bug fixes for home page and divided home view into three separate views

--- public/project/client/views/home/home-popular.controller.js
+++ public/project/client/views/home/home-popular.controller.js
@@ -13,20 +13,10 @@
             vm.paginationCounter = 1;
 
             MovieService
-                .getGenreList()
-                .then(function (response) {
-                    var map = new Object();
-                    response.data.genres.forEach(function (element, index, array) {
-                        map[element.id] = element.name;
-                    });
-                    vm.genreList = map;
-                });
-
-            MovieService
                 .findPopularMovies(vm.paginationCounter)
                 .then(
                     function (response) {
-                        var movies = preprocessResponse(response);
+                        var movies = $scope.homeControllerModel.preprocessResponse(response);
                         if (movies.length != 0) {
                             vm.movies = movies;
                             vm.error = null;
@@ -52,45 +42,13 @@
                     .findPopularMovies(vm.paginationCounter)
                     .then(
                         function (response) {
-                            var movies = preprocessResponse(response);
+                            var movies = $scope.homeControllerModel.preprocessResponse(response);
                             if (movies.length != 0) {
                                 vm.movies.push.apply(vm.movies, movies);
                                 vm.busy = false;
                             }
                         });
             }
-        }
-
-        function preprocessResponse(response) {
-            response.data.results.forEach(function (element1, index1, array1) {
-                var genres = [];
-                if (element1.genre_ids.length != 0 && element1.genre_ids) {
-                    element1.genre_ids.forEach(function (element2, index2, array2) {
-                        genres.push("#" + getValue(element2));
-                    });
-                }
-                else {
-                    genres.push("#NA");
-                }
-                element1.genres = genres;
-
-                if (element1.backdrop_path) {
-                    element1.imageUrl = $scope.homeControllerModel.imageUrl + element1.backdrop_path;
-                }
-                else {
-                    element1.imageUrl = "/project/client/images/Image-Not-Available.jpg";
-                }
-
-                if (!element1.overview) {
-                    element1.overview = "There is no overview for this movie.";
-                }
-            });
-
-            return response.data.results;
-        }
-
-        function getValue(key) {
-            return vm.genreList[key];
         }
     }
 
--- public/project/client/views/home/home.controller.js
+++ public/project/client/views/home/home.controller.js
@@ -8,6 +8,7 @@
         var vm = this;
 
         vm.slide = slide;
+        vm.preprocessResponse = preprocessResponse;
 
         var slides = [];
 
@@ -18,6 +19,16 @@
 
             var imageUrl = MovieService.getImageURL();
             vm.imageUrl = imageUrl.substring(0, imageUrl.length - 1);
+
+            MovieService
+                .getGenreList()
+                .then(function (response) {
+                    var map = new Object();
+                    response.data.genres.forEach(function (element, index, array) {
+                        map[element.id] = element.name;
+                    });
+                    vm.genreList = map;
+                });
 
             MovieService
                 .findUpcomingMovies()
@@ -38,6 +49,44 @@
         function slide(dir) {
             $('#myCarousel').carousel(dir);
         };
+
+        function preprocessResponse(response) {
+            response.data.results.forEach(function (element1, index1, array1) {
+                var genres = [];
+                if (element1.genre_ids.length != 0 && element1.genre_ids) {
+                    element1.genre_ids.forEach(function (element2, index2, array2) {
+                        try {
+                            var genreName = getValue(element2);
+                            genres.push("#" + genreName);
+                        }
+                        catch (err) {
+
+                        }
+                    });
+                }
+                else {
+                    genres.push("#NA");
+                }
+                element1.genres = genres;
+
+                if (element1.backdrop_path) {
+                    element1.imageUrl = vm.imageUrl + element1.backdrop_path;
+                }
+                else {
+                    element1.imageUrl = "/project/client/images/Image-Not-Available.jpg";
+                }
+
+                if (!element1.overview) {
+                    element1.overview = "There is no overview for this movie.";
+                }
+            });
+
+            return response.data.results;
+        }
+
+        function getValue(key) {
+            return vm.genreList[key];
+        }
     }
 
 })();
